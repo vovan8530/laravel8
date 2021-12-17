d438aa4494c4fb779b888d3337cfea34bea2fe7a
vovan8530 <vovan.b8530@gmail.com>
2021-12-17 13:13:18 +0200

    change data seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder {
    * @return void
    */
   public function run() {
-    \App\Models\User::factory(5)->create();
+//    \App\Models\User::factory(5)->create();
     $this->call([
       CategorySeeder::class,
       SubCategorySeeder::class,
